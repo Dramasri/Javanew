58c5dfae43b9ff1219c9af28183b1b3f4d1821ae
Dramasri <dramaapr01@gmail.com>
2020-01-22 15:14:38 +0530

    hai

---
+++ src/org/java/Java.java
@@ -0,0 +1,17 @@
+package org.java;
+
+public class Java {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hai");
+		
+		System.out.println("hello");
+		
+		
+		
+	}
+
+}
